e973ac48a8ccae1f47eb20f4a599715d3a650a3b
RJ <crongjie@hotmail.com>
2016-11-28 03:17:28 +0900

    first commit

---
+++ index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import { ReactNativePropRegistry } from 'react-native';
+import React, { Component } from 'react';
+import { Container, Content, Badge } from 'native-base';
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+
+export default class ReactNativeBaseTest extends Component {
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <Badge>2</Badge>
+                    <Badge primary>2</Badge>
+                    <Badge success>2</Badge>
+                    <Badge info>2</Badge>
+                    <Badge warning>2</Badge>
+                    <Badge danger>2</Badge>
+                    <Badge
+                      style={{ backgroundColor: 'black' }}
+                      textStyle={{ color: 'white' }}>
+                        1866
+                    </Badge>
+                </Content>
+            </Container>
+        );
+    }
+}
+AppRegistry.registerComponent('ReactNativeBaseTest', () => ReactNativeBaseTest);
