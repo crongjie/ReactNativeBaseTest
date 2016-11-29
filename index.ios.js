abf45dfc5439b66182a8d55386167c9c08b4f5f3
陳 荣傑 <wingkit.chan@bizreach.co.jp>
2016-11-29 18:23:02 +0900

    added form

--- index.ios.js
+++ index.ios.js
@@ -5,18 +5,35 @@
  */
 import { ReactNativePropRegistry } from 'react-native';
 import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Icon, Badge } from 'native-base';
 import {
   AppRegistry,
   StyleSheet,
   View
 } from 'react-native';
 
+import { Container, Content, List, ListItem, InputGroup, Input, Text, Picker, Header, Title, Footer, FooterTab, Button, Icon, Badge } from 'native-base';
+const Item = Picker.Item;
+
 function hello_world() {
     alert('Hello world!')
 }
 
 export default class ReactNativeBaseTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedItem: undefined,
+            selected1: 'key0',
+            results: {
+                items: [],
+            },
+        };
+    }
+    onValueChange(value: string) {
+        this.setState({
+            selected1: value,
+        });
+    }
     render() {
         return (
             <Container>
@@ -25,17 +42,67 @@ export default class ReactNativeBaseTest extends Component {
                 </Header>
 
                 <Content>
-                    <Badge>2</Badge>
-                    <Badge primary>2</Badge>
-                    <Badge success>2</Badge>
-                    <Badge info>2</Badge>
-                    <Badge warning>2</Badge>
-                    <Badge danger>2</Badge>
-                    <Badge
-                      style={{ backgroundColor: 'black' }}
-                      textStyle={{ color: 'white' }}>
-                        1866
-                    </Badge>
+                    <List>
+                       <ListItem iconLeft>
+                            <Icon name="ios-person" style={{ color: '#0A69FE' }} />
+                            <Text>First Name</Text>
+                            <Input inlineLabel label="First Name" placeholder="John" />
+                            <Button style={{backgroundColor: '#384850'}} >
+                                <Icon name='ios-search' style={{color: '#00c497'}}/>
+                            </Button>
+                            <Picker
+                              iosHeader="Select one"
+                              mode="dropdown"
+                              selectedValue={this.state.selected1}
+                              onValueChange={this.onValueChange.bind(this)} >
+                                <Item label="Chan" value="key0" />
+                                <Item label="Lee" value="key1" />
+                                <Item label="Zhang" value="key2" />
+                            </Picker>
+                        </ListItem>
+
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-person" style={{ color: '#0A69FE' }} />
+                                <Input placeholder="EMAIL" />
+                            </InputGroup>
+                        </ListItem>
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-unlock" style={{ color: '#0A69FE' }} />
+                                <Input placeholder="PASSWORD" secureTextEntry />
+                            </InputGroup>
+                        </ListItem>
+                        <ListItem>
+                            <InputGroup>
+                                <Icon name="ios-call" style={{ color: '#0A69FE' }} />
+                                <Input placeholder="PHONE" keyboardType="numeric" />
+                            </InputGroup>
+                        </ListItem>
+
+                        <ListItem iconLeft>
+                            <Icon name="ios-transgender" style={{ color: '#0A69FE' }} />
+                            <Text>GENDER</Text>
+                            <Picker
+                              iosHeader="Select one"
+                              mode="dropdown"
+                              selectedValue={this.state.selected1}
+                              onValueChange={this.onValueChange.bind(this)} >
+                                <Item label="Male" value="key0" />
+                                <Item label="Female" value="key1" />
+                                <Item label="Other" value="key2" />
+                            </Picker>
+                        </ListItem>
+                    
+                        <ListItem>
+                            <InputGroup >
+                                <Input stackedLabel label="Permanent Address" placeholder="Address" />
+                            </InputGroup>
+                        </ListItem>
+                    </List>
+                    <Button style={{ alignSelf: 'center', marginTop: 20, marginBottom: 20 }}>
+                        Sign Up
+                    </Button>
                 </Content>
 
                 <Footer>
